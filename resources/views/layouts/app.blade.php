08f3045045a8d9eda0153d13701eb9541b7fa311
Andreas Nevlacsil <a.nevlacsil@hotmail.com>
2022-03-18 18:08:23 +0100

    print color white

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -27,6 +27,14 @@
     <!-- Font Awesome JS -->
     <script defer src="https://use.fontawesome.com/releases/v5.0.13/js/solid.js" integrity="sha384-tzzSw1/Vo+0N5UhStP3bvwWPq+uvzCMfrN1fEFe+xBmv1C/AtVX5K0uZtmcHitFZ" crossorigin="anonymous"></script>
     <script defer src="https://use.fontawesome.com/releases/v5.0.13/js/fontawesome.js" integrity="sha384-6OIrr52G08NpOFSZdxxz1xdNSndlD4vdcf/q2myIUVO0VsqaGHJsB0RaBE01VTOY" crossorigin="anonymous"></script>
+    <style>
+        @media print {
+            body {
+                background-color: #fff;
+            }
+        }
+    </style>
+
 </head>
 
 <body>
@@ -61,7 +69,7 @@
                         </li>
                         @endif
                         @else
-                        
+
                         <li class="nav-item">
                             <a class="nav-link" href="/members" style="color: #f39200!important; font-weight:bold">Members</a>
                         </li>
